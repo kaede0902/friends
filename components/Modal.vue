78d87090ca195764c309fc48a8bb5f18e37e3663
kaede0902 <kaede0920js@gmail.com>
2021-08-30 01:39:53 +0900

    add template Modal component

---
+++ components/Modal.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-dialog
+    max-width="290"
+  >
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn
+        color="primary"
+        dark
+        v-bind="attrs"
+        v-on="on"
+      >
+        Open Dialog
+      </v-btn>
+    </template>
+
+    <v-card>
+      <v-card-title class="text-h5">
+        Friend Detail
+      </v-card-title>
+    </v-card>
+
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: 'Modal',
+}
+</script>
